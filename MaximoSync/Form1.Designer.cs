e71da79737130c2f076acd63050b67e12613d51e
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-04-18 17:23:51 -0300

    Desenvolvimento

--- MaximoSync/Form1.Designer.cs
+++ MaximoSync/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.Windows.Forms.Label tempoMinutosLabel;
             System.Windows.Forms.Label urlLinkLabel;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txtTempo = new System.Windows.Forms.TextBox();
             this.txtUrlLink = new System.Windows.Forms.TextBox();
             this.btnSincronizar = new System.Windows.Forms.Button();
@@ -41,7 +42,7 @@
             // 
             tempoMinutosLabel.AutoSize = true;
             tempoMinutosLabel.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            tempoMinutosLabel.Location = new System.Drawing.Point(37, 81);
+            tempoMinutosLabel.Location = new System.Drawing.Point(37, 69);
             tempoMinutosLabel.Name = "tempoMinutosLabel";
             tempoMinutosLabel.Size = new System.Drawing.Size(87, 17);
             tempoMinutosLabel.TabIndex = 1;
@@ -50,7 +51,7 @@
             // txtTempo
             // 
             this.txtTempo.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTempo.Location = new System.Drawing.Point(130, 78);
+            this.txtTempo.Location = new System.Drawing.Point(130, 66);
             this.txtTempo.Name = "txtTempo";
             this.txtTempo.Size = new System.Drawing.Size(158, 24);
             this.txtTempo.TabIndex = 2;
@@ -59,7 +60,7 @@
             // 
             urlLinkLabel.AutoSize = true;
             urlLinkLabel.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            urlLinkLabel.Location = new System.Drawing.Point(88, 42);
+            urlLinkLabel.Location = new System.Drawing.Point(88, 30);
             urlLinkLabel.Name = "urlLinkLabel";
             urlLinkLabel.Size = new System.Drawing.Size(36, 17);
             urlLinkLabel.TabIndex = 3;
@@ -68,7 +69,7 @@
             // txtUrlLink
             // 
             this.txtUrlLink.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUrlLink.Location = new System.Drawing.Point(130, 39);
+            this.txtUrlLink.Location = new System.Drawing.Point(130, 27);
             this.txtUrlLink.Name = "txtUrlLink";
             this.txtUrlLink.Size = new System.Drawing.Size(158, 24);
             this.txtUrlLink.TabIndex = 4;
@@ -76,7 +77,7 @@
             // btnSincronizar
             // 
             this.btnSincronizar.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSincronizar.Location = new System.Drawing.Point(130, 119);
+            this.btnSincronizar.Location = new System.Drawing.Point(130, 107);
             this.btnSincronizar.Name = "btnSincronizar";
             this.btnSincronizar.Size = new System.Drawing.Size(100, 30);
             this.btnSincronizar.TabIndex = 5;
@@ -87,14 +88,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(368, 173);
+            this.ClientSize = new System.Drawing.Size(365, 151);
             this.Controls.Add(this.btnSincronizar);
             this.Controls.Add(urlLinkLabel);
             this.Controls.Add(this.txtUrlLink);
             this.Controls.Add(tempoMinutosLabel);
             this.Controls.Add(this.txtTempo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "MaximoSync";
             this.ResumeLayout(false);
             this.PerformLayout();
 
