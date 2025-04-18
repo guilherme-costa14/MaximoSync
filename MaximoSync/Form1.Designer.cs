6c5f4975eae34a3a1010b15e001fdf60dec299f7
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-04-18 17:20:56 -0300

    Renomeando os componentes
    Renomeando os componentes que será usado no código.

--- MaximoSync/Form1.Designer.cs
+++ MaximoSync/Form1.Designer.cs
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label tempoMinutosLabel;
+            System.Windows.Forms.Label urlLinkLabel;
+            this.txtTempo = new System.Windows.Forms.TextBox();
+            this.txtUrlLink = new System.Windows.Forms.TextBox();
+            this.btnSincronizar = new System.Windows.Forms.Button();
+            tempoMinutosLabel = new System.Windows.Forms.Label();
+            urlLinkLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tempoMinutosLabel
+            // 
+            tempoMinutosLabel.AutoSize = true;
+            tempoMinutosLabel.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            tempoMinutosLabel.Location = new System.Drawing.Point(37, 81);
+            tempoMinutosLabel.Name = "tempoMinutosLabel";
+            tempoMinutosLabel.Size = new System.Drawing.Size(87, 17);
+            tempoMinutosLabel.TabIndex = 1;
+            tempoMinutosLabel.Text = "Tempo (min):";
+            // 
+            // txtTempo
+            // 
+            this.txtTempo.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTempo.Location = new System.Drawing.Point(130, 78);
+            this.txtTempo.Name = "txtTempo";
+            this.txtTempo.Size = new System.Drawing.Size(158, 24);
+            this.txtTempo.TabIndex = 2;
+            // 
+            // urlLinkLabel
+            // 
+            urlLinkLabel.AutoSize = true;
+            urlLinkLabel.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            urlLinkLabel.Location = new System.Drawing.Point(88, 42);
+            urlLinkLabel.Name = "urlLinkLabel";
+            urlLinkLabel.Size = new System.Drawing.Size(36, 17);
+            urlLinkLabel.TabIndex = 3;
+            urlLinkLabel.Text = "URL:";
+            // 
+            // txtUrlLink
+            // 
+            this.txtUrlLink.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUrlLink.Location = new System.Drawing.Point(130, 39);
+            this.txtUrlLink.Name = "txtUrlLink";
+            this.txtUrlLink.Size = new System.Drawing.Size(158, 24);
+            this.txtUrlLink.TabIndex = 4;
+            // 
+            // btnSincronizar
+            // 
+            this.btnSincronizar.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSincronizar.Location = new System.Drawing.Point(130, 119);
+            this.btnSincronizar.Name = "btnSincronizar";
+            this.btnSincronizar.Size = new System.Drawing.Size(100, 30);
+            this.btnSincronizar.TabIndex = 5;
+            this.btnSincronizar.Text = "Sincronizar";
+            this.btnSincronizar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(368, 173);
+            this.Controls.Add(this.btnSincronizar);
+            this.Controls.Add(urlLinkLabel);
+            this.Controls.Add(this.txtUrlLink);
+            this.Controls.Add(tempoMinutosLabel);
+            this.Controls.Add(this.txtTempo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtTempo;
+        private System.Windows.Forms.TextBox txtUrlLink;
+        private System.Windows.Forms.Button btnSincronizar;
     }
 }
 
---
+++ MaximoSync/Formulario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximoSync
+{
+    public class Formulario
+    {
+        public string urlLink { get; set; }
+        public decimal tempoMinutos { get; set; }
+    }
+}
