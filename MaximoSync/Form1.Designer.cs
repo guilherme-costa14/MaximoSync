e9870f0484930dd2e263b5784feec29a65ce58d3
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-04-18 17:24:44 -0300

    desenvolvimento

--- MaximoSync/Form1.Designer.cs
+++ MaximoSync/Form1.Designer.cs
@@ -88,7 +88,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(365, 151);
+            this.ClientSize = new System.Drawing.Size(365, 160);
             this.Controls.Add(this.btnSincronizar);
             this.Controls.Add(urlLinkLabel);
             this.Controls.Add(this.txtUrlLink);
