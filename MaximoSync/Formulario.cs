dfe82d272f1dedeecdd0cf904e79581aeeaf8c11
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-04-18 21:26:58 -0300

    Método json
    Adicionando o método json para trabalhar com serialização e desserialização.

--- MaximoSync/Formulario.cs
+++ MaximoSync/Formulario.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json; //Permite fazer a serialização para .json
 
 namespace MaximoSync
 {
